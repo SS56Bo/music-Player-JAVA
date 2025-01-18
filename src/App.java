ccc72438674037aa8001cf133f91395c86fce6ca
Soham Sarkar <109454662+SS56Bo@users.noreply.github.com>
2025-01-19 01:05:42 +0530

    Update App.java

--- src/App.java
+++ src/App.java
@@ -4,10 +4,10 @@ public class App extends JFrame {
     public static void main(String[] args) {
         JFrame jframe = new JFrame("Hello World");
 
-        jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE); 
+        jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE); // close the java program upon pressing the exit button
 
-        jframe.setSize(650, 600);
+        jframe.setSize(650, 600); // setSize (width, height) (What a weird way to frame, lol)
 
-        jframe.setVisible(true);
+        jframe.setVisible(true); // for perpetual keeping it 
     }
 }
