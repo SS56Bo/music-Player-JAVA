f8b57ab530215ba61ad7939b0ae26a4af1b8ebc4
SS56Bo <sohamsarkar560@gmail.com>
2025-01-19 00:59:25 +0530

    First WINDOW: Implementation

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javax.swing.JFrame;
+
+public class App extends JFrame {
+    public static void main(String[] args) {
+        JFrame jframe = new JFrame("Hello World");
+
+        jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE); 
+
+        jframe.setSize(650, 600);
+
+        jframe.setVisible(true);
     }
 }
