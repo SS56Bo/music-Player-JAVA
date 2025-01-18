83abf6f8d57f5a55be0244bb97b3506ebb4edb97
SS56Bo <sohamsarkar560@gmail.com>
2025-01-19 01:17:02 +0530

    Button Component: Added

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class App extends JFrame {
@@ -7,6 +8,12 @@ public class App extends JFrame {
         jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE); // close the java program upon pressing the exit button
 
         jframe.setSize(650, 600); // setSize (width, height) (What a weird way to frame, lol)
+
+        //for button
+        JButton button = new JButton("Press me");
+
+        //add the button
+        jframe.getContentPane().add(button);
 
         jframe.setVisible(true); // for perpetual keeping it 
     }
