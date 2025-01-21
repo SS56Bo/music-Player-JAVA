e4aef27babcfda05ca80347b13c718369fbce183
Soham Sarkar <109454662+SS56Bo@users.noreply.github.com>
2025-01-21 22:06:00 +0530

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,10 +2,12 @@ import javax.swing.SwingUtilities;
 
 public class App {
     public static void main(String[] args) {
+    /* sets up a Java Swing application with SwingUtilities.invokeLater to ensure that the GUI creation 
+        happens on the Event Dispatch Thread (EDT) */
        SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new MusicPlayerGUI().setVisible(false);
+                new MusicPlayerGUI().setVisible(true);
             }
        });
     }
